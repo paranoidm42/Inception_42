11be32317b03eeadca407ed7f38673688c4664ee
paranoid_m42 <cemlur125@gmai.com>
2024-05-30 22:24:25 +0300

    42

--- srcs/requirements/mariadb/tools/initial_db.sh
+++ srcs/requirements/mariadb/tools/initial_db.sh
@@ -3,17 +3,19 @@
 
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > /initial_db.sql
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> /initial_db.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> /initial_db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> /initial_db.sql
+echo "GRANT ALL   ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> /initial_db.sql
 echo "FLUSH PRIVILEGES;" >> /initial_db.sql
 
 service mysql start
 
-until mysqladmin ping -h localhost >/dev/null 2>&1; do
-  echo "Waiting for MariaDB to be fully started..."
-  sleep 2
-done
+if mysql < /initial_db.sql; then
+  echo "Database and user created"
+else
+  echo "Something went wrong but don't wory if wordpress work!!"
+fi
 
-mysql < /initial_db.sql
 
-exec mysqld
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+
+mysqld
